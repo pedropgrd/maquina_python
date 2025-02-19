85f20712d52391d632ae5b8dcd05fdc10b6c80ed
pedropgrd <pedro.haf23@gmail.com>
2025-02-19 17:30:52 -0400

    v9

--- maq_agricola/home/models.py
+++ maq_agricola/home/models.py
@@ -1,9 +1,4 @@
 from django.db import models
-from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.models import User
-from django.contrib import messages
-
 
 class Anuncio(models.Model):
     CATEGORIAS = (
@@ -20,6 +15,7 @@ class Anuncio(models.Model):
     modelo = models.CharField(max_length=50)
     ano = models.IntegerField()
     preco = models.DecimalField(max_digits=10, decimal_places=2)
+    telefone_contato = models.CharField(max_length=15, blank=True, null=True)
     imagem = models.ImageField(upload_to='imagens_anuncios/', blank=True, null=True)
     data_publicacao = models.DateTimeField(auto_now_add=True)
 
@@ -30,5 +26,3 @@ class Anuncio(models.Model):
 
     def __str__(self):
         return self.titulo
-
-
